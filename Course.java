25907120dda2497858226961996f37aa07f7ae26
SeyidMuhammedKahraman <75672493+KahramanSeyid@users.noreply.github.com>
2022-02-22 22:55:12 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,12 @@
+public class Course {
+    public Course(int id,String name,String mentor,double progress){
+        this.id=id;
+        this.name = name;
+        this.mentor=mentor;
+        this.progress = progress;
+    }
+    int id ;
+    String name;
+    String mentor;
+    double progress;
+}
---
+++ CourseManager.java
@@ -0,0 +1,9 @@
+public class CourseManager {
+    public void addToCourse(Course course){
+        System.out.println(course.name+ " Kurs eklendi.");
+    }
+    
+    public void deleteCourse(Course course){
+        System.out.println(course.name+" Kurs silindi...");
+    }
+}
